46df52e0bc9e6f7b7120cefb55157dd854efa8bc
TedPhillips1 <ted.phillips11@btinternet.com>
2024-03-26 17:54:09 +0000

    added navbar

--- components/custom/Navbar.tsx
+++ components/custom/Navbar.tsx
@@ -3,6 +3,13 @@
 import React from 'react';
 import { usePathname } from 'next/navigation';
 import menuItems from '@/lib/constants/menuItems';
+import { Input } from '../ui/input';
+import { Bell } from 'lucide-react';
+import { Button } from '../ui/button';
+
+const tempNotifications = {
+    amount: 4,
+};
 
 const getTitleFromPath = (pathname: string) => {
     for (const menuItem of menuItems) {
@@ -21,8 +28,20 @@ const Navbar = () => {
 
     return (
         <header className='panel w-full my-4 flex flex-row justify-between'>
-            <h1 className='text-lg'>{pageTitle}</h1>
-            <div>search and more</div>
+            <h1 className='text-lg m-auto mx-0'>{pageTitle}</h1>
+            <div className='flex flex-row justify-end gap-4'>
+                <Input
+                    type='search'
+                    placeholder='Search'
+                    className='bg-slate-900 border-slate-500'
+                />
+                <Button variant='ghost'>
+                    Notifications{'  '}
+                    <span className='ml-2 text-xs p-1 rounded-full w-6 h-6 bg-orange-600 text-slate-50'>
+                        {tempNotifications.amount}
+                    </span>
+                </Button>
+            </div>
         </header>
     );
 };
